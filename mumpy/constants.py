75ff81248d5aa3b2d6478b63766a07538f12718c
Ian Ling <iling@datto.com>
2018-12-19 15:59:31 -0800

    Add permissions enum

--- mumpy/constants.py
+++ mumpy/constants.py
@@ -38,6 +38,30 @@ class MessageType(metaclass=MumpyEnum):
     SUGGESTCONFIG = 25
 
 
+class Permission(metaclass=MumpyEnum):
+    NONE = 0x0
+    WRITE = 0x1
+    TRAVERSE = 0x2
+    ENTER = 0x4
+    SPEAK = 0x8
+    MUTE_DEAFEN = 0x10
+    MOVE = 0x20
+    MAKE_CHANNEL = 0x40
+    LINK_CHANNEL = 0x80
+    WHISPER = 0x100
+    TEXT_MESSAGE = 0x200
+    MAKE_TEMP_CHANNEL = 0x400
+
+    # Root channel only
+    KICK = 0x10000
+    BAN = 0x20000
+    REGISTER = 0x40000
+    SELF_REGISTER = 0x80000
+
+    CACHED = 0x8000000
+    ALL = 0xf07ff
+
+
 class AudioType:
     CELT_ALPHA = 0
     PING = 1
